268031a2cbbcf6f9429aa59b5d41e7f2643b58c2
Tesao <shimeji9@gmail.com>
2021-10-10 18:13:52 +0900

    不要なテストを削除およびコメントアウト

--- spec/models/point_spec.rb
+++ spec/models/point_spec.rb
@@ -19,6 +19,6 @@
 #
 require 'rails_helper'
 
-RSpec.describe Point, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe Point, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
