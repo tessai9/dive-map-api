664bb8ac1d846b482e653deb89b47f0fcd4fae0e
Tesao <shimeji9@gmail.com>
2021-12-23 21:01:18 +0900

    ログインテストの修正

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -3,7 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Sessions', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { create :user, password: 'password' }
+  let(:valid_params) do
+    {
+      email: user.email,
+      password: 'password'
+    }
+  end
+  let(:invalid_params) do
+    {
+      email: 'invalid@example.com',
+      password: 'invalid_password'
+    }
+  end
+
+  describe 'POST /login' do
+    it 'with valid parameters' do
+      post login_path, params: { session: valid_params }
+      json_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(200)
+      expect(json_data[:result]).to eq('ok')
+    end
+
+    it 'with invalid parameters' do
+      post login_path, params: { session: invalid_params }
+
+      expect(response.status).to eq(422)
+    end
   end
 end
